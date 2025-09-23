c43cc99e4d70f19f87cec371dfc09c6145792695
wasif <muhammad.wasif7595@gmail.com>
2025-09-23 06:09:07 +0500

    home page structure no footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   initMarquee("#marquee", 1);
   initMarquee("#marquee2", -1);
+  initSwiper();
 
-  const altElements = document.querySelectorAll([".heading-style-h1", "h1", ".heading-style-h2", "h2", ".navbar ul li a"]);
+  const altElements = document.querySelectorAll([
+    ".heading-style-h1",
+    "h1",
+    ".heading-style-h2",
+    "h2",
+    ".navbar ul li a",
+  ]);
 
   altElements.forEach((el) => {
-    el.innerHTML = el.textContent.replace(/([CGJSQ123569])/gi, '<span class="alt-font">$1</span>');
+    el.innerHTML = el.textContent.replace(
+      /([CGJSQ123569])/gi,
+      '<span class="alt-font">$1</span>'
+    );
   });
 });
 
@@ -17,7 +27,6 @@ function initMarquee(selector, speed = 1) {
   function animate() {
     position -= speed;
     if (Math.abs(position) >= inner.firstElementChild.offsetWidth + 32) {
-      // ek item nikal kar end me daal do taake infinite lage
       inner.appendChild(inner.firstElementChild);
       position = 0;
     }
@@ -25,4 +34,21 @@ function initMarquee(selector, speed = 1) {
     requestAnimationFrame(animate);
   }
   animate();
+}
+
+function initSwiper() {
+  new Swiper(".mySwiper", {
+    slidesPerView: "5",
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".mySwiper .swiper-button-next",
+      prevEl: ".mySwiper .swiper-button-prev",
+    },
+  });
 }
